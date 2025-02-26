bc6591df6a1882028dccf51d005644f7fe61d60b
Adrien Carpentier <me@adriencarpentier.com>
2025-02-26 20:28:33 +0900

    feat: add eyes_001 algorithm

--- settings.ts
+++ settings.ts
@@ -4,4 +4,4 @@ export const CANVAS_HEIGHT = 960
 
 // Current algorithm to display
 // Change this value to switch between algorithms
-export const CURRENT_ALGORITHM = "mondrian"
+export const CURRENT_ALGORITHM = "eyes_001"
