ee8962255c456a4c5e8e2729e694cb10cf227dfd
Adrien Carpentier <me@adriencarpentier.com>
2025-02-28 15:36:50 +0900

    feat: add chea algorithm

--- settings.ts
+++ settings.ts
@@ -4,11 +4,11 @@ export const CANVAS_HEIGHT = 960
 
 // Current algorithm to display
 // Change this value to switch between algorithms
-export const CURRENT_ALGORITHM = "golden"
+export const CURRENT_ALGORITHM = "chea"
 
 // Color palette to use
 // This is completely independent from the algorithm
-export const COLOR_PALETTE = "golden"
+export const COLOR_PALETTE = "chea"
 
 export const MOVIES_PALETTES_API_URL =
 	"https://movies-palettes.adriencarpentier.com/api"
